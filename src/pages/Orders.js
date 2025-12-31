f196f97de0e9c37097551ac8e15b5df7c0c21133
Ranjith Gadhasu <ranjithgadhasu@gmail.com>
2025-12-31 10:30:56 +0530

    Initial commit

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   GridComponent,
   ColumnsDirective,
@@ -12,18 +12,20 @@ import {
   PdfExport,
   Edit,
   Inject,
-} from "@syncfusion/ej2-react-grids";
+} from '@syncfusion/ej2-react-grids';
 
-import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
-import { Header } from "../components";
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Orders = () => {
   const editing = { allowDeleting: true, allowEditing: true };
+
   return (
-    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Page" title="Orders" />
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Orders' />
+
       <GridComponent
-        id="gridcomp"
+        id='gridcomp'
         dataSource={ordersData}
         allowPaging
         allowSorting
@@ -33,11 +35,19 @@ const Orders = () => {
         editSettings={editing}
       >
         <ColumnsDirective>
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
           {ordersGrid.map((item, index) => (
-            <ColumnDirective key={index} {...item} />
+            <ColumnDirective
+              key={index}
+              field={item.field}
+              headerText={item.headerText}
+              width={item.width}
+              textAlign={item.textAlign}
+              format={item.format}
+              template={item.template}
+            />
           ))}
         </ColumnsDirective>
+
         <Inject
           services={[
             Resize,
@@ -54,4 +64,5 @@ const Orders = () => {
     </div>
   );
 };
+
 export default Orders;
